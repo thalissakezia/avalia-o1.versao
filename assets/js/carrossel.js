472d560eb799ce44327322d300e0b5515a07f9a9
Thauan Silva <dev.magary@gmail.com>
2022-06-30 06:39:47 -0300

    Base Inicial do Projeto
    Contem: Navbar, Jumbo, Card-Caroulsel, Footer(Links, Sobre e Parceirias), Assistente

---
+++ assets/js/carrossel.js
@@ -0,0 +1,28 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoWidth: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 2,
+    },
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
+
